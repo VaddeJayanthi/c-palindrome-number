6f2a09e988233c881bf54e31f37bfff37bc33fcc
VaddeJayanthi <vjayanti689@gmail.com>
2026-09-24 11:06:50 +0530

    Add palindrome checking program in C

---
+++ palindrome.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, reverse = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n /= 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome\n");
+    else
+        printf("Not a palindrome\n");
+
+    return 0;
+}
+
+OUTPUT:
+Enter a number: 876
+Not a palindrome
